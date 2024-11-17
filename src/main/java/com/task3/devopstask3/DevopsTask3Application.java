9956ef3bca8a3ebfb957c4451570a298fb6c6a34
Tatsiana Drabovich <tania.drab@gmail.com>
2024-11-18 00:18:40 +0100

    First draft.

---
+++ src/main/java/com/task3/devopstask3/DevopsTask3Application.java
@@ -0,0 +1,13 @@
+package com.task3.devopstask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTask3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTask3Application.class, args);
+    }
+
+}
