dd0bcd199b9c3081d8b4e916dc0c15d0d33f6987
Tatsiana Drabovich <tania.drab@gmail.com>
2024-11-18 11:06:21 +0100

    Add unused import.

--- src/main/java/com/task3/devopstask3/controllers/DemoController.java
+++ src/main/java/com/task3/devopstask3/controllers/DemoController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class DemoController {
