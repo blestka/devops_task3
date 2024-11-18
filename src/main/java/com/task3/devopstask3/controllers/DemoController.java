b457984579c3a23e395c3ae8fb170067255c4d70
Tatsiana Drabovich <tania.drab@gmail.com>
2024-11-18 13:23:40 +0100

    Add checkstyle and spotbugs plugins.

--- src/main/java/com/task3/devopstask3/controllers/DemoController.java
+++ src/main/java/com/task3/devopstask3/controllers/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public final class DemoController {
     @GetMapping("/devops")
     public String getDevopsName() {
         return Optional.ofNullable(System.getenv("DEVOPS"))
